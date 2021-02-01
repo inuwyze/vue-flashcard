6acfaa9106f0cae82102c4d7be679842b7d9afbe
paul lhungdim <paullhungdim7@gmail.com>
2021-02-01 20:32:10 +0530

    prototype

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,69 @@
 <template>
-  <div id="nav">
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view/>
-</template>
+  <router-view/> -->
+  <div>
+    lol
+    <card :toggle='toggle' :slidedir='slidedir'>
+    </card>
+    <div id="naviBut">
 
+    <div class='left arrow' @click="toggle=!toggle;slidedir='rtl'"></div>
+    <div class='right arrow' @click="toggle=!toggle;slidedir='ltr'"></div>
+    </div>
+    
+  </div>
+</template>
+<script>
+import card from './components/card'
+export default {
+  components: {card},
+  data () {
+    return {
+      toggle:false,
+    }
+  }
+}
+
+</script>
+@import '~styles/main.scss';
 <style lang="scss">
+$lft-rot: rotateZ(45deg);
+$rht-rot: rotateZ(-45deg);
+#naviBut{
+  display: flex;
+  justify-content: space-around;
+  margin: 20px auto auto auto;
+  width: 100px;
+}
+.arrow{
+  width: 10px;
+  height: 10px;
+  border-bottom: 3px solid black;
+  // border-right: 1px solid black;
+  
+  &.left{
+    
+    // background: green;
+    border-left: 3px solid black;
+    transform: $lft-rot;
+  }
+
+  &.right{
+    border-right: 3px solid black;
+    transform: $rht-rot;
+  }
+
+}
+
+.left:hover{
+  transform: scale(1.2)  $lft-rot; 
+}
+.right:hover{
+  transform: scale(1.2)  $rht-rot; 
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/card.vue
@@ -0,0 +1,130 @@
+<template>
+    
+        <transition :name="slidedir" mode="out-in" class="container">
+            <div 
+            v-if="toggle"
+            >
+            <div class="card "
+            :class="{rotate:onHover}"
+            @click="onHover=!onHover">
+                <div class="front centered">
+                    front1
+                </div>
+                <div class="back centered">
+                    back1
+                </div>
+            </div>
+            </div>
+            <div 
+            key="2"
+            
+            v-else>
+                <div class="card"
+                :class="{rotate:onHover}"
+                @click="onHover=!onHover">
+                <div class="front centered">
+                    front2
+                </div>
+                <div class="back centered">
+                    back2
+                </div>
+            </div>
+            </div>
+        </transition>
+</template>
+
+<script>
+export default {
+    props:['toggle','slidedir'],
+    data () {
+        return {
+            onHover:false,   
+            // toggle:false,
+        }
+    
+    },
+    watch:{
+        toggle(){
+            this.onHover=false;
+        }
+    }
+}
+</script>
+
+<style lang='scss'>
+.centered{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.container{
+    display: flex;
+    justify-content: center;
+    height: 50vh;
+    perspective: 500px;
+}
+.front{
+    position: absolute;
+    backface-visibility: hidden;
+    width: 100%;
+    height: 100%;
+}
+.back{
+    position: absolute;
+    backface-visibility: hidden;
+
+    transform:rotateX(180deg) ;
+    width: 100%;
+    height: 100%;
+}
+.card{
+    transform-style: preserve-3d;
+    position: relative;
+    display: flex;
+    /* border:1px solid black; */
+    box-shadow: none;
+    width: 50vw;
+    height: 50vh;
+    /* min-width: 60vw; */
+    background: pink;
+    /* max-width: 80vw; */
+    box-shadow: 2px 3px 10px 0px  black  !important;
+
+transition: transform 1s ease-in-out,box-shadow 0.8s linear;
+    /* padding: 10px; */
+}
+
+.rotate{
+    transform: rotateX(180deg);
+    box-shadow: 2px -3px 10px 0px  black  !important;
+}
+
+.rtl-enter-active,
+.rtl-leave-active,
+.ltr-enter-active,
+.ltr-leave-active
+{
+  transition: opacity 0.40s linear,transform 0.40s linear;
+}
+
+.rtl-enter-from,
+.rtl-leave-to, 
+.ltr-enter-from,
+.ltr-leave-to 
+{
+    /* transform: translateX(100px); */
+    opacity: 0;
+}
+.rtl-leave-to,.ltr-enter-from{
+    transform: translateX(-500px)scale(0.7);
+    
+}
+.rtl-enter-from,.ltr-leave-to{
+    transform: translateX(500px) scale(0.7);
+    
+}
+
+
+
+
+</style>
