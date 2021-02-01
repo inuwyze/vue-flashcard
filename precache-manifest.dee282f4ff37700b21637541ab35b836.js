0f89001a907db22d234001aeb03928e88930b6c5
qwtfy <52367492+qwtfy@users.noreply.github.com>
2021-02-01 16:31:06 +0000

    Deploying to gh-pages from  @ a27fb00cd8bb15474a3f37b9936026922d906d28 🚀

---
+++ precache-manifest.dee282f4ff37700b21637541ab35b836.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb648574d8f151ae8f67",
+    "url": "/vue-flashcard/css/app.fd614290.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-flashcard/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "28e21221a8adbbd9a6f5396aee76fff7",
+    "url": "/vue-flashcard/index.html"
+  },
+  {
+    "revision": "110f419df10653d99692",
+    "url": "/vue-flashcard/js/about.71bdfa6f.js"
+  },
+  {
+    "revision": "cb648574d8f151ae8f67",
+    "url": "/vue-flashcard/js/app.e6b06e91.js"
+  },
+  {
+    "revision": "40b57c4826a091fd4582",
+    "url": "/vue-flashcard/js/chunk-vendors.0bb9f71e.js"
+  },
+  {
+    "revision": "d09d05c0d41341c54db7ec2a1a8e5f7c",
+    "url": "/vue-flashcard/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-flashcard/robots.txt"
+  }
+]);
